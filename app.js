a6ba5e0f7d9b7c8a1f07db238fdfbdd9fa4bb581
= <adigangwar1@gmail.com>
2025-10-08 18:29:40 +0530

    adding new email credentials

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ main()
     console.log("Connected to db ");
   })
      .catch((err) => {
-    console.log("Err in connecting", err);
+    console.log("Err in  connecting to db ", err);
   });
 
 async function main() {
